002d126f6a0a610124e7c820f3068ca1fbed3e61
SteveRuble <SteveRuble@users.noreply.github.com>
2018-12-17 13:15:06 -0500

    added task creator

--- cmd/git.go
+++ cmd/git.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"github.com/google/go-github/v20/github"
 	"github.com/naveego/bosun/pkg"
@@ -21,8 +22,38 @@ var gitCmd = &cobra.Command{
 	Short: "Git commands.",
 }
 
+const(
+	ArgGitBody = "body"
+	ArgGitTaskParentOrg = "parent-org"
+	ArgGitTaskParentRepo = "parent-repo"
+)
+
+
+
+func init() {
+
+	gitCmd.AddCommand(gitTaskCmd)
+	gitTaskCmd.Flags().StringP(ArgGitBody, "m", "", "Issue body.")
+	gitTaskCmd.Flags().String(ArgGitTaskParentOrg, "naveegoinc", "Parent org.")
+	gitTaskCmd.Flags().String(ArgGitTaskParentRepo, "stories", "Parent repo.")
+
+	rootCmd.AddCommand(gitCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// gitCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// gitCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+
+
 var gitTaskCmd = &cobra.Command{
-	Use:   "task {story number} {task name}",
+	Use:   "task {parent-number} {task name}",
 	Args: cobra.ExactArgs(2),
 	Short: "Creates a task in the current repo for the story, and a branch for that task.",
 	Long:"Requires github hub tool to be installed (https://hub.github.com/).",
@@ -33,9 +64,12 @@ var gitTaskCmd = &cobra.Command{
 		viper.BindPFlags(cmd.Flags())
 		
 		currentDir, _ := os.Getwd()
-		segs := filepath.SplitList(currentDir)
-		org, repo := segs[len(segs) - 2], segs[len(segs) - 1]
-		
+		repo := filepath.Base(currentDir)
+		org := filepath.Base(filepath.Dir(currentDir))
+		fmt.Println(org, repo)
+
+		parentOrg := viper.GetString(ArgGitTaskParentOrg)
+		parentRepo := viper.GetString(ArgGitTaskParentRepo)
 
 		storyNumber, err := strconv.Atoi(strings.Trim(args[0], "#"))
 		taskName := args[1]
@@ -56,43 +90,45 @@ var gitTaskCmd = &cobra.Command{
 
 		client := github.NewClient(tc)
 
-		story, _, err := client.Issues.Get(ctx, "naveegoinc", "stories", storyNumber)
+		story, _, err := client.Issues.Get(ctx, parentOrg, parentRepo, storyNumber)
 		if err != nil {
 			return errors.Wrap(err, "get issue")
 		}
 
-
-
+		dumpJSON("story", story)
 
 		body := viper.GetString(ArgGitBody)
 		issueRequest := &github.IssueRequest{
 			Title: github.String(taskName),
-			Body: github.String(fmt.Sprintf("%s\n\nrequired by naveegoinc/stories#%d", body, storyNumber)),
+			Body: github.String(fmt.Sprintf("%s\n\nrequired by %s/%s#%d", body, parentOrg, parentRepo, storyNumber)),
 		}
 		if story.Assignee != nil {
 			issueRequest.Assignee = story.Assignee.Name
 		}
 		if story.Milestone != nil {
 			milestones, _, err := client.Issues.ListMilestones(ctx, org, repo, nil)
+			dumpJSON("milestones", milestones)
+
 			if err != nil {
 				return err
 			}
 			for _, m := range milestones {
-				if m.Title == story.Milestone.Title {
+				if *m.Title == *story.Milestone.Title {
+					pkg.Log.WithField("title", *m.Title).Info("Attaching milestone.")
 					issueRequest.Milestone = m.Number
 					break
 				}
 			}
 		}
 
-		pkg.Log.Info("Creating issue.")
+		dumpJSON("creating issue", issueRequest)
 
 		issue, _, err := client.Issues.Create(ctx, org, repo, issueRequest)
 		if err != nil {
 			return errors.Wrap(err, "creating issue")
 		}
 
-		issueNumber := issue.Number
+		issueNumber := *issue.Number
 		pkg.Log.WithField("issue", issueNumber).Info("Created issue.")
 
 		slug := strings.Replace(strings.ToLower(taskName), " ", "-", -1)
@@ -113,26 +149,9 @@ var gitTaskCmd = &cobra.Command{
 	},
 }
 
-const(
-	ArgGitBody = "body"
-)
-
-
-
-func init() {
-
-	gitCmd.AddCommand(gitTaskCmd)
-	gitTaskCmd.Flags().StringP(ArgGitBody, "m", "", "Issue body.")
-
-	rootCmd.AddCommand(gitCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// gitCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// gitCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+func dumpJSON(label string, data interface{}) {
+	if viper.GetBool(ArgGlobalVerbose) {
+		j, _ := json.MarshalIndent(data, "", "  ")
+		fmt.Fprintf(os.Stderr, "%s:\n%s\n\n", label, string(j))
+	}
 }
